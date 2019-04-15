1c4f2ef2cf8dd6addcce84e6220f50e48cb3d3a4
FWL <1748481785@qq.com>
2019-04-15 15:45:41 +0800

    123

--- demo/ToolBox/Stroke weather/Setstroke.swift
+++ demo/ToolBox/Stroke weather/Setstroke.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2019 com.niit.ios. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Setstroke: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let screen = UIScreen.main.bounds
+        self.view.backgroundColor = #colorLiteral(red: 0.9068111181, green: 0.9017875791, blue: 0.906077683, alpha: 1)
+        
+        let navigationBarHeight:CGFloat = 44
+        let navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 20+10, width: screen.size.width, height: navigationBarHeight))
+        navigationBar.backgroundColor = UIColor.white
+        
+//        let backButtonItem = UIBarButtonItem(title:"返回",style: .plain, target: self, action: #selector(back(_ :)))
+//        backButtonItem.tintColor = UIColor.red
+//
+//        let saveButtonItem = UIBarButtonItem(title: "地图",style: .plain, target: self, action: #selector(save(_ :)))
+        saveButtonItem.tintColor = UIColor.red
+        
+        let navigationItem = UINavigationItem(title: "设置行程")
+        navigationItem.leftBarButtonItem = backButtonItem
+        navigationItem.rightBarButtonItem = saveButtonItem
+        
+        navigationBar.items = [navigationItem]
+        self.view.addSubview(navigationBar)
+
+}
+    
+}
