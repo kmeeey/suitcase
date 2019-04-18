7619fa8d5baea79af10b5482f9380f6ef2f14812
PangPye <835501665@qq.com>
2019-04-18 17:22:21 +0800

    123

---
+++ demo/My/Register.swift
@@ -0,0 +1,116 @@
+//
+//  ViewController.swift
+//  translate
+//
+//  Created by niit97 on 2019/4/1.
+//  Copyright © 2019 niit97. All rights reserved.
+//
+
+import UIKit
+
+class Register: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = #colorLiteral(red: 0.9410971999, green: 0.9412290454, blue: 0.9410556555, alpha: 1)
+        // Do any additional setup after loading the view, typically from a nib.
+        let screen = UIScreen.main.bounds
+        
+        //1. 创建NavigationBar
+        let navigationBarHeight: CGFloat = 44//默认高度
+        let navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 30, width: screen.size.width, height: navigationBarHeight))
+        //设置按钮
+        let leftbtn =  UIBarButtonItem(title:"返回", style:UIBarButtonItem.Style.plain, target: self, action:nil)
+        let rightbtn = UIBarButtonItem(title: "登录", style: .plain, target: self, action: nil)
+        //按钮文字颜色
+        leftbtn.tintColor = UIColor.red
+        rightbtn.tintColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        self.navigationItem.leftBarButtonItem = leftbtn
+        //设置图片按钮
+        //leftbtn.image = UIImage(named: "arrow")
+        let navigationItem = UINavigationItem(title:"注册会员")
+        navigationItem.leftBarButtonItem = leftbtn
+        navigationItem.rightBarButtonItem = rightbtn
+        navigationBar.items = [navigationItem]
+        self.view.addSubview(navigationBar)
+        
+        let icon1 = UIImageView(frame: CGRect.init(x: 50, y: 0, width: 50, height: 50))
+        let image1 = UIImage(named: "phone 13-31-08-190")
+        icon1.image = image1
+        
+        let icon2 = UIImageView(frame: CGRect.init(x: 50, y: 0, width: 50, height: 50))
+        let image2 = UIImage(named: "psw-icon 13-32-25-238")
+        icon2.image = image2
+        
+        let icon3 = UIImageView(frame: CGRect.init(x: 50, y: 0, width: 50, height: 50))
+        let image3 =  UIImage(named: "login_page_fragment_register_password_icon")
+        icon3.image = image3
+        
+        let button = UIButton(frame:CGRect(x:0, y:0, width:100, height:40))
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        button.layer.masksToBounds = true
+        button.setTitle("获取验证码", for:.normal) //普通状态下的文字
+        button.setTitleColor(UIColor.red, for: .normal) //普通状态下文字的颜色
+        button.setTitleColor(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), for: .highlighted) //触摸状态下文字的颜色
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 15) //按钮文字大小
+        
+        
+        let username  = UITextField.init(frame: CGRect.init(x: 0, y:  120, width: self.view.bounds.size.width, height: 45))
+        
+        username.borderStyle =  .none
+        //当文本超出文字框宽度时，自动调整文字大小，默认是以省略号代替
+        username.adjustsFontSizeToFitWidth = true
+        username.minimumFontSize = 10
+        //右侧的x关闭按钮
+        username.clearButtonMode = .always
+        username.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        //左侧图片显示
+        username.leftView = icon1
+        username.leftViewMode = .always
+        username.placeholder = "手机号码"
+        //设置文本框关联的键盘类型
+        //username.keyboardType = UIKeyboardType.numberPad
+        //使文本框在界面打开时就获取焦点，并弹出键盘
+        //username.becomeFirstResponder()
+        //当文本框失去焦点，收回键盘
+        //username.resignFirstResponder()
+        self.view.addSubview(username)
+        
+        let psw = UITextField.init(frame: CGRect.init(x:0, y: username.frame.origin.y + username.frame.size.height, width: self.view.frame.width, height: 50))
+        psw.borderStyle = .none
+        psw.placeholder = "验证码"
+        psw.backgroundColor =  #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        psw.leftViewMode = .always
+        psw.leftView = icon2
+        psw.rightView = button
+        psw.rightViewMode = .always
+        self.view.addSubview(psw)
+        
+        
+        let apsw = UITextField.init(frame: CGRect.init(x: 0, y: psw.frame.origin.y + psw.frame.size.height  , width: self.view.frame.width, height: 50))
+        apsw.isSecureTextEntry = true
+        //右侧的x关闭按钮
+        apsw.clearButtonMode = .always
+        apsw.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        apsw.placeholder = "输入密码"
+        apsw.leftView =  icon3
+        apsw.leftViewMode = .always
+        apsw.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        self.view.addSubview(apsw)
+        
+        
+        let runle1 =  UIView.init(frame: CGRect.init(x: 55, y:username.frame.origin.y + username.frame.size.height , width: self.view.bounds.width, height: 1))
+        runle1.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        self.view.addSubview(runle1)
+        
+        let runle2 =  UIView.init(frame: CGRect.init(x: 55, y:
+            psw.frame.origin.y + psw.frame.size.height, width: view.bounds.width, height: 1))
+        runle2.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        self.view.addSubview(runle2)
+        
+    }
+    
+    
+}
---
+++ demo/Travel/MyTrip.swift
@@ -0,0 +1,129 @@
+//
+//  IndexViewController.swift
+//  demo
+//
+//  Created by 朱信磊 on 2019/4/3.
+//  Copyright © 2019 com.niit.ios. All rights reserved.
+//
+
+import UIKit
+
+class MyTrip: UIViewController,UITableViewDataSource,UITableViewDelegate{
+    
+    var dataSource = [[String:String]()]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //获取屏幕
+        let screen = UIScreen.main.bounds
+        self.view.backgroundColor =  #colorLiteral(red: 0.9763445258, green: 0.976619184, blue: 0.967718184, alpha: 1)
+        let navigationBarHeight: CGFloat = 44//默认高度
+        //        let navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: screen.size.width, height: navigationBarHeight))
+        //        //设置导航栏标题名字
+        //        let navigationItem = UINavigationItem(title:"游记")
+        self.navigationItem.title = "游记"
+        //        navigationBar.items = [navigationItem]
+        //        self.view.addSubview(navigationBar)
+        
+        //设置空的控件
+        let view1 = UIView(frame: CGRect(x: 0, y: 0,width: screen.size.width, height: 50))
+        view1.backgroundColor = #colorLiteral(red: 0.999904573, green: 1, blue: 0.999872148, alpha: 1)
+        self.view.addSubview(view1)
+        //左边字体设置
+        let label1 = UILabel(frame: CGRect(x: 60, y: (view1.frame.size.height - 30)/2, width: 100, height: 30))
+        label1.text = "全部城市"
+        label1.textColor = #colorLiteral(red: 0.6822943687, green: 0.6823918819, blue: 0.6822634339, alpha: 1)
+        label1.font = UIFont(name: "Zapfino", size: 18)
+        view1.addSubview(label1)
+        //右边字体设置
+        let label2 = UILabel(frame: CGRect(x: screen.size.width/2 + 60 , y: (view1.frame.size.height - 30)/2, width: 100, height: 30))
+        label2.text = "默认排序"
+        label2.textColor = #colorLiteral(red: 0.6822943687, green: 0.6823918819, blue: 0.6822634339, alpha: 1)
+        label2.font = UIFont(name: "Zapfino", size: 18)
+        view1.addSubview(label2)
+        //分割线
+        let line =  UIView.init(frame: CGRect.init(x:screen.size.width/2, y: 10, width: 1.5, height: 30))
+        line.backgroundColor = #colorLiteral(red: 0.6822943687, green: 0.6823918819, blue: 0.6822634339, alpha: 1)
+        view1.addSubview(line)
+        
+        self.view.backgroundColor = UIColor.white;
+        self.navigationItem.title = "游记";
+        
+        
+        let view2 = UIView.init(frame:CGRect.init(x:0,y:view1.frame.size.height + 0,width:self.view.frame.size.width,height:
+            230))
+        view2.backgroundColor = UIColor.red
+        self.view.addSubview(view2)
+        
+        let tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.backgroundColor = UIColor.white;
+        view2.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        dataSource = [
+            ["title":"徽州古城游，记一笔生活的味道",
+             "icon":"huizhou-icon",
+             "icon2":"login_page_fragment_center_head",
+             "label2":"12","label3":"993","label4":"0"],
+            ["title":"徽州古城游，记一笔生活的味道",
+             "icon":"huizhou-icon",
+             "icon2":"login_page_fragment_center_head",
+             "label2":"2222","label3":"993","label4":"0"],
+            ["title":"徽州古城游，记一笔生活的味道",
+             "icon":"huizhou-icon",
+             "icon2":"login_page_fragment_center_head",
+             "label2":"222","label3":"222","label4":"0"],
+            ["title":"徽州古城游，记一笔生活的味道",
+             "icon":"huizhou-icon",
+             "icon2":"login_page_fragment_center_head",
+             "label2":"0","label3":"443","label4":"0"],
+            ["title":"徽州古城游，记一笔生活的味道",
+             "icon":"huizhou-icon",
+             "icon2":"login_page_fragment_center_head",
+             "label2":"0","label3":"664","label4":"0"],
+            ["title":"徽州古城游，记一笔生活的味道",
+             "icon":"huizhou-icon",
+             "icon2":"login_page_fragment_center_head",
+             "label2":"0","label3":"993","label4":"0"],
+            ["title":"徽州古城游，记一笔生活的味道",
+             "icon":"huizhou-icon",
+             "icon2":"login_page_fragment_center_head",
+             "label2":"0","label3":"338","label4":"0"]
+        ]
+    }
+    
+    //MARK: UITableViewDataSource
+    // cell的个数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    // UITableViewCell 基本样式
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellid = "testCellID"
+        var cell:NewTableViewCell? = tableView.dequeueReusableCell(withIdentifier: cellid) as? NewTableViewCell
+        if cell == nil {
+            cell = NewTableViewCell(style: .subtitle, reuseIdentifier: cellid)
+        }
+        let dict:Dictionary = dataSource[indexPath.row]
+        cell?.icon.image = UIImage(named: dict ["icon"]!)
+        cell?.icon1.image = UIImage(named: dict ["icon2"]!)
+        cell?.label.text = dict["title"]
+        cell?.label2.text  = dict["label2"]
+        cell?.label3.text  = dict["label3"]
+        cell?.label4.text  = dict["label4"]
+        return cell!
+    }
+    
+    //MARK: UITableViewDelegate
+    // 设置cell高度
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 230
+    }
+    // 选中cell后执行此方法
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+    
+}
