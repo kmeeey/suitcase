1e399b60ea1ca49b924ed48bb7b13190bf39cc3c
YinRui <123@123.com>
2019-04-18 16:17:15 +0800

    Assitant模块

--- demo/Assistant/Assistant.swift
+++ demo/Assistant/Assistant.swift
@@ -1,22 +1,24 @@
 //
-//  ViewController.swift
-//  navigation
+//  Assistant.swift
+//  demo2
 //
-//  Created by abc on 2019/4/3.
+//  Created by abc on 2019/4/17.
 //  Copyright © 2019 abc. All rights reserved.
 //
 
 import UIKit
 
 class Assistant: UIViewController {
-
+    
+//    let demo = demoView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        view.addSubview(demo)
         
         //加载试图后进行任何其他设置
         self.navigationItem.title = "行程助手"
-        self.view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        self.view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         
         //设置左上角导航按钮的样式和功能
         let leftItem = UIBarButtonItem(title: "行程", style: .plain, target: self, action: #selector(leftPage))
@@ -28,49 +30,57 @@ class Assistant: UIViewController {
         rightItem.tintColor = UIColor.gray
         self.navigationItem.rightBarButtonItem = rightItem
         
+        //图片
+        let image = UIImageView(frame: CGRect(x: self.view.frame.maxX / 4, y: self.view.frame.maxY / 7, width: 200, height: 220))
+        let icon = UIImage(named: "account_assistant_nodata")
+        image.image = icon
+        self.view.addSubview(image)
         
-       
+        //创建行程按钮
+        let btn_CreateJourney = UIButton(type: .system)
+        //设置按钮的大小和位置
+        btn_CreateJourney.frame = CGRect(x: (self.view.frame.maxX / 4) + 17.5, y: self.view.frame.maxY / 2.3, width: 165, height: 45)
+        //设置按钮的背景图片
+        btn_CreateJourney.setBackgroundImage(UIImage(named: "btn_journey"), for: .normal)
+        //把图片添加到view中
+        view.addSubview(btn_CreateJourney)
+        //设置按钮的点击事件
+        btn_CreateJourney.addTarget(self, action: #selector(creatJourney), for: .touchUpInside)
+        
+        
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @objc func creatJourney() {
+        
+        //初始化第二个视图控制器对象
+        let creatJourney = CreatJourney()
+        //将第二个视图控制器压入导航视图控制器，实现页面跳转
+        self.navigationController?.pushViewController(creatJourney,animated: true)
+    }
+    
     
     //按钮的点击事件
     @objc func leftPage() {
         //初始化第二个视图控制器对象
-        let viewController1 = CreateTrip()
+        let trippage = TripPage()  //行程
         //将第二个视图控制器压入导航视图控制器，实现页面跳转
-    self.navigationController?.pushViewController(viewController1,animated: true)
-}
+        self.navigationController?.pushViewController(trippage,animated: true)
+    }
+    
+    
     
     @objc func rightPage() {
         //初始化第二个视图控制器对象
-        let viewController2 = CreateTrip()
+        let listpage = ListPage()  //列表
         //将第二个视图控制器压入导航视图控制器，实现页面跳转
-        self.navigationController?.pushViewController(viewController2,animated: true)
+        self.navigationController?.pushViewController(listpage,animated: true)
     }
     
     
-    
-    //视图即将可见时执行该方法
-    //导航栏的样式设置
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        //设置是否透明
-        //self.navigationController?.navigationBar.isTranslucent = false
-        //设置系统样式
-       //self.navigationController?.navigationBar.barStyle = .black
-        //设置背景样式
-       //self.navigationController?.navigationBar.barTintColor = UIColor.orange
-    }
-    
-    
-//let imageView = UIImageView(image: UIImage(named: "image"))
-//    imageView.frame = CGRect(x:10, y:30, width:300, height:150)
-//    self.view.addSubview(imageView)
-//
-//
-//    let path = Bundle.main.path(forResource: "image", ofType: "png")
-//    let newImage = UIImage(contentsOfFile: path!)
-//    let imageView = UIImageView(image:newImage)
-//    self.view.addSubview(imageView)
     
 }
 
---
+++ demo/Assistant/TripPage.swift
@@ -0,0 +1,24 @@
+//
+//  TripPage.swift
+//  demo2
+//
+//  Created by abc on 2019/4/17.
+//  Copyright © 2019 abc. All rights reserved.
+//
+
+import UIKit
+class TripPage: UIViewController {
+    var isHideNavBar: Bool = false
+    var isHideToolBar: Bool = false
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //加载视图后进行任何其他设置
+        self.title = "行程"
+        self.view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        
+        
+    }
+}
+
